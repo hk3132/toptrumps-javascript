bdeefe625c4cf584b8b853cb99144e498da09cdf
hk3132 <57266582+hk3132@users.noreply.github.com>
2019-11-04 16:39:58 +0000

    Add files via upload

---
+++ toptrumps.js
@@ -0,0 +1,215 @@
+let africaAnimal = [
+    [animal1 = "Giraffe", speed = 60, strength = 60, compassion = 70, likeability = 100, aggression = 20],
+    [animal2 = "Ardvark", speed = 40, strength = 20, compassion = 80, likeability = 95, aggression = 70],
+    [animal3 = "Baboon", speed = 30, strength = 70, compassion = 40, likeability = 10, aggression = 85],
+    [animal4 = "Cheetah", speed = 120, strength = 80, compassion = 15, likeability = 60, aggression = 75],
+    [animal5 = "Elephant", speed = 25, strength = 90, compassion = 95, likeability = 80, aggression = 35],
+    [animal6 = "Warthog", speed = 48, strength = 40, compassion = 55, likeability = 25, aggression = 30],
+    [animal7 = "Meerkat", speed = 32, strength = 10, compassion = 90, likeability = 90, aggression = 25],
+    [animal8 = "Hippopotamus", speed = 30, strength = 100, compassion = 25, likeability = 75, aggression = 100],
+    [animal9 = "Impala", speed = 80, strength = 50, compassion = 30, likeability = 5, aggression = 10],
+    [animal10 = "Lion", speed = 80, strength = 85, compassion = 30, likeability = 55, aggression = 75],
+    [animal11 = "Porcupine", speed = 3, strength = 5, compassion = 10, likeability = 10, aggression = 80],
+    [animal12 = "Rhino", speed = 50, strength = 85, compassion = 10, likeability = 40, aggression = 90],
+    [animal13 = "Topi", speed = 70, strength = 50, compassion = 30, likeability = 10, aggression = 30],
+    [animal14 = "Wildebeest", speed = 80, strength = 65, compassion = 25, likeability = 5, aggression = 35],
+    [animal15 = "Flamingo", speed = 60, strength = 25, compassion = 90, likeability = 95, aggression = 10],
+    [animal16 = "Mongoose", speed = 32, strength = 70, compassion = 10, likeability = 25, aggression = 90],
+    [animal17 = "Zebra", speed = 64, strength = 10, compassion = 70, likeability = 80, aggression = 10],
+    [animal18 = "Chimpanzee", speed = 40, strength = 75, compassion = 65, likeability = 30, aggression = 55],
+    [animal19 = "Hyena", speed = 60, strength = 45, compassion = 5, likeability = 5, aggression = 75],
+    [animal20 = "Ostrich", speed = 70, strength = 15, compassion = 5, likeability = 55, aggression = 5],
+    [animal21 = "Leopard", speed = 58, strength = 65, compassion = 25, likeability = 45, aggression = 80],
+    [animal22 = "Serval", speed = 80, strength = 15, compassion = 30, likeability = 45, aggression = 65],
+    [animal23 = "Vulture", speed = 100, strength = 15, compassion = 5, likeability = 5, aggression = 75],
+    [animal24 = "Gazelle", speed = 97, strength = 45, compassion = 5, likeability = 5, aggression = 75],
+    [animal25 = "Gorilla", speed = 40, strengh = 95, compassion = 65, likeability = 30, aggression = 65],
+    [animal26 = "Dikdik", speed = 42, strengh = 20, compassion = 45, likeability = 60, aggression = 20],
+    [animal27 = "Okapi", speed = 60, strength = 35, compassion = 75, likeability = 80, aggression = 10],
+    [animal28 = "Mosquito", speed = 2, strength = 5, compassion = 1, likeability = 5, aggression = 95],
+    [animal29 = "Crocodile", speed = 32, strength = 90, compassion = 10, likeability = 5, aggression = 95],
+    [animal30 = "Bonobo", speed = 40, strength = 65, compassion = 40, likeability = 25, aggression = 45],
+]
+
+let southAmericanAnimal = [
+    [animal1 = "Galapagos Tortoise", speed = 2, strength = 60, compassion = 90, likeability = 75, aggression = 5],
+    [animal2 = "Anaconda", speed = 2, strength = 80, compassion = 5, likeability = 5, aggression = 80],
+    [animal3 = "Capybara", speed = 35, strength = 10, compassion = 75, likeability = 85, aggression = 10],
+    [animal4 = "Piranha", speed = 40, strength = 30, compassion = 5, likeability = 5, aggression = 95],
+    [animal5 = "Condor", speed = 56, strength = 70, compassion = 20, likeability = 60, aggression = 65],
+    [animal6 = "Llama", speed = 56, strength = 40, compassion = 90, likeability = 100, aggression = 5],
+    [animal7 = "Caiman", speed = 48, strength = 80, compassion = 10, likeability = 5, aggression = 90],
+    [animal8 = "Tapir", speed = 48, strength = 20, compassion = 80, likeability = 90, aggression = 30],
+    [animal9 = "Lemur", speed = 20, strength = 10, compassion = 95, likeability = 95, aggression = 15],
+    [animal10 = "Chinchilla", speed = 24, strength = 5, compassion = 80, likeability = 10, aggression = 40],
+    [animal11 = "Anteater", speed = 49, strength = 20, compassion = 85, likeability = 100, aggression = 15],
+    [animal12 = "Armadillo", speed = 48, strength = 95, compassion = 50, likeability = 80, aggression = 50],
+    [animal13 = "Iguana", speed = 33, strength = 10, compassion = 5, likeability = 5, aggression = 70],
+    [animal14 = "Rhea", speed = 65, strength = 15, compassion = 10, likeability = 40, aggression = 30],
+    [animal15 = "Mara", speed = 29, strength = 5, compassion = 75, likeability = 60, aggression = 45],
+    [animal16 = "Jaguar", speed = 80, strength = 65, compassion = 30, likeability = 70, aggression = 60],
+    [animal17 = "Toucan", speed = 19, strength = 10, compassion = 70, likeability = 80, aggression = 5],
+    [animal18 = "Macaw", speed = 56, strength = 10, compassion = 70, likeability = 40, aggression = 5],
+    [animal19 = "Coati", speed = 27, strength = 15, compassion = 40, likeability = 80, aggression = 20],
+    [animal20 = "Lion Tamarin", speed = 40, strength = 5, compassion = 85, likeability = 90, aggression = 5],
+    [animal21 = "Ocelot", speed = 61, strength = 60, compassion = 20, likeability = 70, aggression = 55],
+    [animal22 = "River Dolphin", speed = 23, strength = 5, compassion = 100, likeability = 65, aggression = 10],
+    [animal23 = "Sloth", speed = 0, strength = 40, compassion = 100, likeability = 100, aggression = 0],
+    [animal24 = "Spider Monkey", speed = 56, strength = 30, compassion = 50, likeability = 55, aggression = 40],
+    [animal25 = "Manatee", speed = 30, strength = 30, compassion = 80, likeability = 95, aggression = 5],
+    [animal26 = "Tarantula", speed = 2, strength = 90, compassion = 5, likeability = 5, aggression = 65],
+    [animal27 = "Cotton Top Tamarin", speed = 40, strength = 5, compassion = 90, likeability = 90, aggression = 5],
+    [animal28 = "Puma", speed = 80, strength = 70, compassion = 10, likeability = 50, aggression = 75],
+    [animal29 = "Alpaca", speed = 56, strength = 75, compassion = 70, likeability = 80, aggression = 20],
+    [animal30 = "Blue Footed Booby", speed = 97, strength = 5, compassion = 10, likeability = 70, aggression = 5]
+]
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function firstTurn() {
+    rNum = Math.floor(Math.random() * 2 + 1)
+}
+firstTurn();
+
+if (rNum == 1) turn = true;
+if (rNum == 2) turn = false;
+
+let person = prompt(`Enter Player 1's name: `)
+let person2 = prompt(`Enter Player 2's name: `)
+let game = prompt(`Hello ${person} and ${person2}! What deck would you like to play with? 1. African Animals or 2. South American Animals? 
+(Enter 1 or 2)`)
+
+if (game == 1) {
+    alert(`Let's play African Animal Top Trumps!`)
+    africaAnimal = shuffle(africaAnimal)
+    playerOneDeck = africaAnimal.slice(0, 15);
+    playerTwoDeck = africaAnimal.slice(15, 30)
+}
+
+if (game == 2) {
+    alert(`Let's play South American Animal Top Trumps!`)
+    southAmericanAnimal = shuffle(southAmericanAnimal)
+    playerOneDeck = southAmericanAnimal.slice(0, 15);
+    playerTwoDeck = southAmericanAnimal.slice(15, 30)
+}
+
+limbo = []
+
+let card1 = ""
+let card2 = ""
+let stat = ""
+let stat2 = ""
+
+
+function drawCard() {
+    while (playerOneDeck.length > 0 && playerTwoDeck.length > 0) {
+        for (i = 0; i < playerOneDeck.length; i++)
+            card1 = playerOneDeck[i]
+        playerOneDeck.pop()
+
+        for (i = 0; i < playerTwoDeck.length; i++)
+            card2 = playerTwoDeck[i]
+        playerTwoDeck.pop();
+
+        if (turn == true) {
+            alert(`${person} has the next turn`)
+            stat = prompt(
+                `${person} has drawn 
+        Animal : ${card1[0]} 
+        1. Speed : ${card1[1]} 
+        2. Strength : ${card1[2]} 
+        3. Compassion : ${card1[3]} 
+        4. Likeability : ${card1[4]} 
+        5. Aggression : ${card1[5]}
+        What stat would you like to select? (Enter Number 1 - 5)`)
+
+            alert(`${person2} has drawn 
+        Animal : ${card2[0]} 
+        1. Speed : ${card2[1]} 
+        2. Strength : ${card2[2]} 
+        3. Compassion : ${card2[3]} 
+        4. Likeability : ${card2[4]} 
+        5. Aggression : ${card2[5]}`)
+        }
+
+        if (turn == false) {
+            alert(`${person2} has the next turn`)
+            stat2 = prompt(
+                `${person2} has drawn 
+        Animal : ${card2[0]} 
+        1. Speed : ${card2[1]} 
+        2. Strength : ${card2[2]} 
+        3. Compassion : ${card2[3]} 
+        4. Likeability : ${card2[4]} 
+        5. Aggression : ${card2[5]}
+        What stat would you like to select? (Enter Number 1 - 5)`)
+
+            alert(`${person} has drawn 
+        Animal : ${card1[0]} 
+        1. Speed : ${card1[1]} 
+        2. Strength : ${card1[2]} 
+        3. Compassion : ${card1[3]} 
+        4. Likeability : ${card1[4]} 
+        5. Aggression : ${card1[5]}`)
+        }
+
+        if (stat == 1 || stat2 == 1) {
+            alert(`We are comparing Speed. ${card1[0]} Speed: ${card1[1]} 
+        vs ${person2} has drawn ${card2[0]} Speed: ${card2[1]}`)
+        } if (stat == 2 || stat2 == 2) {
+            alert(`We are comparing Strength. ${card1[0]} Strength: ${card1[2]} 
+        vs ${person2} has drawn ${card2[0]} Strength: ${card2[2]}`)
+        } if (stat == 3 || stat2 == 3) {
+            alert(`We are comparing Compassion. ${card1[0]} Compassion: ${card1[3]} 
+        vs ${person2} has drawn ${card2[0]} Compassion: ${card2[3]}`)
+        } if (stat == 4 || stat2 == 4) {
+            alert(`We are comparing Likeability. ${card1[0]} Likeability: ${card1[4]} 
+        vs ${person2} has drawn ${card2[0]} Likeability: ${card2[4]}`)
+        } if (stat == 5 || stat2 == 5) {
+            alert(`We are comparing Agression. ${card1[0]} Aggression: ${card1[5]} 
+        vs ${person2} has drawn ${card2[0]} Aggression: ${card2[5]}`)
+        }
+
+        if (card1[stat] > card2[stat] || card1[stat2] > card2[stat2]) {
+            playerOneDeck.unshift(card2), playerOneDeck.unshift(card1); alert(`${card1[0]} beats ${card2[0]}. ${person} WINS.`);
+            if (limbo.length > 0) {
+            playerOneDeck = limbo.concat(playerOneDeck), limbo = []; alert(`${person} has won all cards in the limbo pile`);
+            }
+            alert(`${person} now has ${playerOneDeck.length} cards, ${person2} now has ${playerTwoDeck.length} cards`)
+            turn = true; stat = "";
+
+        } else if (card2[stat] > card1[stat] || card2[stat2] > card1[stat2]) {
+            playerTwoDeck.unshift(card1), playerTwoDeck.unshift(card2); alert(`${card2[0]} beats ${card1[0]}. ${person2} WINS.`)
+            if (limbo.length > 0) {
+                playerTwoDeck = limbo.concat(playerTwoDeck); limbo = []; alert(`${person2} has won all cards in the limbo pile`)
+            }
+            alert(`${person} now has ${playerOneDeck.length} cards, ${person2} now has ${playerTwoDeck.length} cards`);
+            turn = false; stat = "";
+
+        }
+        else if (card1[stat] == card2[stat] || card1[stat2] == card2[stat2]) {
+            limbo.push(card2), limbo.push(card1)
+            alert(`${card1[0]} and ${card2[0]} are evenly matched!!!! There are now ${limbo.length} cards in the limbo pile.`)
+            alert(`${person} now has ${playerOneDeck.length} cards, ${person2} now has ${playerTwoDeck.length} cards`);
+            turn = !turn; stat = "";
+
+        }
+    }
+    alert(`You win!`)
+}
+
+drawCard();
